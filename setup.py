f95adcdd305127b9c9444f90252f10f5ce92362c
Patil, Parikshit <parikshit.patil@siemens.com>
2021-12-24 19:24:28 +0530

    small change

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Logging handler',
+        'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
     ],
